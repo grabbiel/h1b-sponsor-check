f46b5c4ac251fc805f066bb790d6233773d13bb4
Francisco Cruzado <90626364+pakitow@users.noreply.github.com>
2023-02-05 20:23:35 -0500

    Add files via upload

---
+++ modules/contents/linkedin/pages/jobsearch.js
@@ -0,0 +1,44 @@
+class SearchPage extends PageManipulation{
+    constructor(documentbody){
+        super();
+        this.body = documentbody;
+        this.main_pane(documentbody);
+        this.top_pane(this.mainpane);
+        this.position_placeholder(this.toppane);
+        this.job_description(this.mainpane).finally(()=>{
+            this.cdescription = this.getDescription();
+            new Messaging({origin: "content", operation: "submit", data:{
+                description: true,
+                content: this.cdescription
+            }});
+        });
+        this.name_placeholder(this.toppane).finally(()=>{
+            this.getName().finally(()=>{
+                new Messaging({origin: "content", operation: "submit", data:{
+                    name: true,
+                    content: this.cname}
+                });
+            });
+        });
+    }
+    main_pane(htmlbody){
+        let mainparent = htmlbody.querySelector("#main");
+        this.mainpane = mainparent.querySelector("div > .jobs-search__job-details > .jobs-search__job-details--container > .jobs-details > div:not([class]) > div");
+    }
+    job_description = async(mainpane) =>{
+        this.descriptionholder =  mainpane.querySelector("#job-details > span");
+    }
+    top_pane(mainpane){
+        this.toppane =  mainpane.querySelectorAll(":scope > div:not([id]) > .jobs-unified-top-card > div")[0];
+    }
+    name_placeholder = async(toppane) =>{
+        this.nameholder = toppane.querySelector(".jobs-unified-top-card__company-name");
+    }
+    position_placeholder(toppane){
+        let positionHeaderAnchor = toppane.querySelector(":scope > [class^='jobs-unified-top-card__content'] > a");
+        positionHeaderAnchor.addEventListener("click", (clickEvent)=>{
+            clickEvent.stopPropagation();
+        });
+        this.positionholder = positionHeaderAnchor.querySelector(":scope > [class*='job']");
+    }
+}
---
+++ modules/contents/linkedin/pages/methods.js
@@ -0,0 +1,19 @@
+class PageManipulationMethods{
+    constructor(){
+    }
+    main_pane(){
+        throw new Error("[ABSTRACT_IMPLEMENTATION]: Method 'main_pane()' must be implemented.");
+    }
+    job_description(){
+        throw new Error("[ABSTRACT_IMPLEMENTATION]: Method 'job_description()' must be implemented.");
+    }
+    top_pane(){
+        throw new Error("[ABSTRACT_IMPLEMENTATION]: Method 'top_pane()' must be implemented.");
+    }
+    name_placeholder(){
+        throw new Error("[ABSTRACT_IMPLEMENTATION]: Method 'name_placeholder()' must be implemented.");
+    }
+    position_placeholder(){
+        throw new Error("[ABSTRACT_IMPLEMENTATION]: Method 'position_placeholder()' must be implemented.");
+    }
+}
