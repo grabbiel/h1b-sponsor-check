7f552abd3467d6dabe6d021e392618c80823c972
Francisco Cruzado <90626364+pakitow@users.noreply.github.com>
2023-02-05 20:24:40 -0500

    Add files via upload

---
+++ modules/contents/badges/company.js
@@ -0,0 +1,69 @@
+class CompanyBadge{
+    constructor(nameContainer, companyStatus){
+        this.container = nameContainer;
+        this.clearCache();
+        this.addListingBadge(companyStatus);
+    }
+    clearCache(){
+        if(this.container.hasChildNodes()){
+            this.#removeDOMElements(this.container.getElementsByTagName("span"));
+            this.#removeDOMElements(this.container.getElementsByTagName("svg"));
+        }
+    }
+    #removeDOMElements(elements_array){
+        if(elements_array!=undefined && elements_array!=null){
+            if(elements_array.length > 0){
+                Array.from(elements_array).forEach((ele)=>{
+                    this.container.removeChild(ele);
+                });
+            }
+        }
+    }
+    addListingBadge(cstatus){
+        const spanSpace = document.createElement("span");
+        spanSpace.innerHTML = " ";
+        this.container.appendChild(spanSpace);
+        this.container.appendChild(this.#createSVG(cstatus));
+    }
+    #createSVG(cstatus){
+        const svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+        svg.style.verticalAlign = "middle";
+        svg.setAttribute("viewBox","0 0 18 18");
+        svg.setAttribute("width","18");
+        svg.setAttribute("height","18");
+        svg.setAttribute("xmlns","http://www.w3.org/2000/svg");
+        svg.appendChild(this.#createPath(cstatus));
+        svg.addEventListener("click",e=>{
+            this.#popupmanager();
+        });
+        return svg;
+    }
+    #createPath(status){
+        const path = document.createElementNS("http://www.w3.org/2000/svg","path");
+        path.style.verticalAlign = "middle";
+        if(status){
+            path.setAttribute("fill","#0FFF00");
+            path.addEventListener("mouseover",e=>{
+                path.setAttribute("fill","#0cb500");
+            });
+            path.addEventListener("mouseout",e=>{
+                path.setAttribute("fill","#0fff00");
+            })
+            path.setAttribute("d", "M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z");
+            return path;
+        }else{
+            path.setAttribute("fill","#FF2D00");
+            path.addEventListener("mouseover",e=>{
+                path.setAttribute("fill","#8f1b01");
+            });
+            path.addEventListener("mouseout",e=>{
+                path.setAttribute("fill","#FF2D00");
+            });
+            path.setAttribute("d", "M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zm-6.106 4.5L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z");
+            return path;
+        }
+    }
+    #popupmanager(){
+        new Messaging({origin: "popup", operation: "request", data:{full: true} });
+    }
+}
---
+++ modules/contents/badges/position.js
@@ -0,0 +1,48 @@
+class PostingBadge{
+    constructor(positionContainer, jobClassification){
+        this.container = positionContainer;
+        this.clearCache();
+        this.#addBadge(jobClassification);
+    }
+    clearCache(){
+        if(this.container.childNodes.length > 1){
+            this.container.removeChild(this.container.lastChild);
+            this.container.removeChild(this.container.lastChild);
+        }
+    }
+    #addBadge(cstatus){
+        const spanSpace = document.createElement("span");
+        spanSpace.innerHTML = " ";
+        this.container.appendChild(spanSpace);
+        this.container.appendChild(this.#createBadge(cstatus));
+    }
+    #createBadge(companystatus){
+        this.cstatus = companystatus;
+        const spanElement = document.createElement("span");
+        spanElement.style =`
+        vertical-align: middle;
+        display: inline-block;
+        font-size: 40%;
+        background-color: #FF2D00;
+        line-height: 50%;
+        color: white; 
+        padding: 1.25% 1.25%; 
+        text-align: center; 
+        border-radius: 7.5%;
+        `;
+        if(this.cstatus){spanElement.style.backgroundColor = "#0FFF00";}
+        const anchor = document.createElement("a");
+        anchor.innerText = "sponsors";
+        anchor.title = "More H1B information";
+        anchor.addEventListener("click",e=>{
+            e.preventDefault();
+            this.#popup_manager();
+            return false;
+        });
+        spanElement.appendChild(anchor);
+        return spanElement;
+    }
+    #popup_manager(){
+        new Messaging({origin: "popup", operation: "request", data:{full: true} });
+    }    
+}
