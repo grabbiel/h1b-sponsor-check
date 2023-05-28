03ab5fffffd635a05c92b24c40b9f6f1e710a171
pakitow <fcruzado@mail.stmarytx.edu>
2023-05-27 19:30:18 -0500

    cleaned

--- modules/background/messaging/mainmessage.js
+++
@@ -1,9 +0,0 @@
-export default class Message{
-    constructor(tabId, origin, operation, data){
-        chrome.tabs.sendMessage(tabId, {
-            origin: origin,
-            operation: operation,
-            data: data
-        });
-    }
-}
